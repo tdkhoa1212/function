851882bd668d667fb220ac1ffcc36dc7f5244f4c
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-12-07 15:53:27 +0700

    Update moving_average.py

--- moving_average.py
+++ moving_average.py
@@ -2,24 +2,26 @@ import numpy as np
 from scipy.io import wavfile
 from ssqueezepy import ssq_cwt
 import matplotlib.pyplot as plt
+import sys
 
 # Get wavelet from .wav file
-def wav2wavelet(path):       
+def wav_to_wavelet(path):
     sample_rate, x = wavfile.read(path) 
-    Twx, Wx, *_ = ssq_cwt(x) #get Wx 
-    return  Wx
+    twx, wx, *_ = ssq_cwt(x)  # use wx
+    print(f'shape: {wx.shape}')
+    return np.abs(wx)
 
 # Moving average function
-def MovAverage(array, window):
+def moving_average(array, window):
     ret = np.cumsum(array, dtype=float)
     ret[window:] = ret[window:] - ret[:-window]
     return ret[window - 1:] / window
 
 # Moving average for wavelet
-def wavelet2MovAverage(matrix, window):
+def wavelet_to_moving_average(matrix, window):
     ma = []
     for i in matrix:
-        i = MovAverage(i, window)
+        i = moving_average(i, window)
         i = np.expand_dims(i, axis=0)
         if ma == []:
             ma = i
@@ -28,13 +30,22 @@ def wavelet2MovAverage(matrix, window):
     return ma
 
 # Plot a chosen row in the matrix
-def plotMA(matrix, row):
+def plot_row(title, matrix, row):
     get = matrix[row]
     plt.plot(get)
-    plt.title('After moving average')
-    plt.show()
+    plt.title(title)
 
 if __name__ == '__main__':
-    a = wav2wavelet('/home/ubuntu-pc/Enrico_boss/voices/wav/the_different_word/good_afternoon_Kendra_Female.wav') 
-    b = wavelet2MovAverage(a, 2)
-    plotMA(b, 100)
+    if len(sys.argv) != 3:
+        print('Usage: python moving_average.py <path to wav file> <row number>')
+        sys.exit(-1)
+
+    wav_file = sys.argv[1]
+    row = int(sys.argv[2])
+    window = 1000
+
+    wavelet = wav_to_wavelet(wav_file)
+    ma = wavelet_to_moving_average(wavelet, window)
+    plot_row('', ma, row)
+    plot_row(f'Row {row} - Orange=source, Blue=averaged({window})', wavelet, row)
+    plt.show()
