1d420a0cee5559faef1d3b06ab0953446e931f0a
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-01-12 22:44:51 +0700

    apply pytest

--- test/test_last_piece.py
+++ test/test_last_piece.py
@@ -123,12 +123,12 @@ dis = 200 # distance between columns in stra function
 
 def test_run():
     for idx, wx in enumerate(wav_to_wavelet(path, time_window)):
-        if idx == 1:
-            break
+        # if idx == 1:
+        #     break
         ma_hist = stairway(wavelet_to_moving_average(wx, window), bins)
         print(f'Shape of stair segment {idx+1}: {ma_hist.shape}')
         ma_hist_stra = stra(ma_hist, dis = dis)
         print(f'Shape of straight segment {idx+1}: {ma_hist_stra.shape}\n')
 
-test_run()
+# test_run()
 
