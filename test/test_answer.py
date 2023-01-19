227698671dd0b55d48f97546ccc94172af0efd07
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-01-19 18:43:41 +0700

    the final testing function

--- test/test_answer.py
+++ test/test_answer.py
@@ -17,18 +17,10 @@ def test_memory():
         current_me = psutil.virtual_memory().percent
         assert past_me-2 < current_me < past_me+2, f"the percentage of used RAM suddently changed, past memory: {past_me}, current memory: {current_me}"
         
-        # Test parameters------------------------------------------------------
-        assert len(wx.shape) == 2, "wx doesn't get 2D-shape, must have a 2D-shape"
-        assert window <= int(wx.shape[1]), f"The value of window must be less than or equal number of colums in the wx matrix, but they are {window} and {int(wx.shape[1])} of window and wx's column respectively"
-        assert dis <= int(wx.shape[1]/2), f"The value of dis should be less than of colums in the wx matrix twice, but they are {dis} and {int(wx.shape[1]/2)} of dis and wx's column respectively"
-        assert bins <= int(wx.shape[1]/2), f"The value of bins should be less than of colums in the wx matrix twice, but they are {bins} and {int(wx.shape[1]/2)} of bins and wx's column respectively"
-
-        # Test histogram ma------------------------------------------------------
         ma_hist = stairway(wavelet_to_moving_average(wx, window), bins) 
         assert wx.shape[0] == ma_hist.shape[0], f"The number of rows in matrices must be equated, but they are {wx.shape[0]} and {ma_hist.shape[0]} of wx and ma_hist respectively"
         print(f'Shape of stair segment {idx+1}: {ma_hist.shape}')
 
-        # Test histogram straighted ma------------------------------------------------------
         ma_hist_stra = stra(ma_hist, dis = dis)
         assert wx.shape[0] == ma_hist_stra.shape[0], f"The number of rows in matrices must be equated, but they are {wx.shape[0]} and {ma_hist_stra.shape[0]} of wx and ma_hist_stra respectively"    
         print(f'Shape of straight segment {idx+1}: {ma_hist_stra.shape}\n')
